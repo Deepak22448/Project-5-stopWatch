e06e4f29285184cb004a87103e2e043612966b9a
Deepak Sharma <deepaksandeep33@gmail.com>
2021-08-25 13:04:38 +0530

    removed waste lines

--- stopwatch.js
+++ stopwatch.js
@@ -1,29 +1,26 @@
 let ms = document.getElementsByClassName("ms")[0];
 let sec = document.getElementsByClassName("sec")[0];
 let min = document.getElementsByClassName("min")[0];
-// console.log(ms, sec, min)
-let minNum = 1;
-let secNum = 1;
-let num = 0;
 
 let interval;
 function myStopWatch() {
+  let minNum = 1;
+  let secNum = 1;
+  let num = 0;
   interval = setInterval(() => {
-    ms.innerHTML = num + 1;
+    ms.innerHTML = ` ${num} ms`;
     num++;
-    if (ms.innerHTML == 100) {
+    if (num == 100) {
       num = 0;
-      ms.innerHTML = num;
-      num++;
+      ms.innerHTML = ` ${num} ms`;
 
-      sec.innerHTML = secNum;
+      sec.innerHTML = ` ${secNum} s`;
       secNum++;
-    } else if (sec.innerHTML >= 60) {
+    } else if (secNum >= 60) {
       secNum = 0;
-      sec.innerHTML = secNum;
-      secNum++;
+      sec.innerHTML = ` ${secNum} s`;
 
-      min.innerHTML = minNum;
+      min.innerHTML = ` ${minNum} m`;
       minNum++;
     }
   }, 10);
@@ -42,10 +39,10 @@ btnStop.addEventListener("click", () => {
 //resart button
 const btnRestart = document.getElementsByClassName("btn-restart")[0];
 btnRestart.addEventListener("click", () => {
-  ms.innerHTML = 0;
-  sec.innerHTML = 0;
-  min.innerHTML = 0;
-  minNum = 1;
-  secNum = 1;
+  ms.innerHTML = 0+'ms';
+  sec.innerHTML = 0+'s';
+  min.innerHTML = 0+'m';
+  minNum = 0;
+  secNum = 0;
   num = 0;
 });
